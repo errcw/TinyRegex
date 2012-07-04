cbd8231b43dabfe0b4c20b52c7fd74a5cef32a02
Eric Woroshow <eric@ericw.ca>
2012-07-04 02:20:03 -0700

    Ditched tokenizer in favor of recursive descent parser with embedded tokenization. In progress.

--- TinyRegex.fs
+++ TinyRegex.fs
@@ -1,33 +1,58 @@
-﻿// TinyRegex: A tiny regular matcher using derivatives in F#
-module TinyRegex
+﻿#light
 
-// A regular expression is:
-// <regex> = <term> '|' <regex>
-//         | <term>
-// <term> = { <factor> }
-// <factor> = <base> [ '*' ]
-// <base> = <char>
-//        | '(' <regex> ')'
+// TinyRegex: A tiny regular matcher using derivatives in F#
+module TinyRegex = 
 
-type Regex =
-    | Empty
-    | Null
-    | Char of char
-    | Alt of Regex * Regex
-    | Seq of Regex * Regex
-    | Rep of Regex * Regex
+    type Regex =
+        | Empty
+        | Char of char
+        | Alt of Regex * Regex
+        | Seq of Regex * Regex
+        | Rep of Regex
 
-type Tokens =
-    | Char of char
-    | LParen
-    | RParen
-    | Star
-    | Pipe
+    // Consumes the given token and returns the rest
+    let (|TK|_|) (token : char) (tokens : char list) = function
+        | t :: rest when t = token -> Some(rest)
+        | _ -> None
 
-let tokenize (input : string) =
-    input |> Seq.map (function
-        | '(' -> LParen
-        | ')' -> RParen
-        | '*' -> Star
-        | '|' -> Pipe
-        | c -> Char c)
+    // Consumes any character and returns the character and the rest
+    let (|CH|_|) tokens = function
+        | t :: rest -> Some(t, rest)
+        | _ -> None
+
+    // A regular expression is:
+    // <regex> = <term> '|' <regex>
+    //         | <term>
+    // <term> = { <factor> }
+    // <factor> = <base> [ '*' ]
+    // <base> = <char>
+    //        | '(' <regex> ')'
+    let rec (|Expr|_|) t =
+        match t with
+        | Term(e1, TK '|' (Expr(e2, rest))) -> Some(Alt(e1, e2), rest)
+        | Term(e, rest) -> Some(e, rest)
+        | _ -> None
+
+    and (|Term|_|) t =
+        match t with
+        | Factor(e1, Factor(e2, rest)) -> Some(Seq(e1, e2), rest)
+        | Factor(e, rest) -> Some(e, rest)
+        | _ -> None
+
+    and (|Factor|_|) t =
+        match t with
+        | Base(e, TK '*' rest) -> Some(Rep e, rest)
+        | Base(e, rest) -> Some(e, rest)
+        | _ -> None
+
+    and (|Base|_|) t =
+        match t with
+        | TK '(' (Expr(e, TK ')' rest)) -> Some(e, rest)
+        | CH(ch, rest) -> Some(Char ch, rest)
+        | _ -> None
+
+
+    // Testing
+    match Seq.toList "(abc" with
+    | Expr(e, []) -> printfn "%A" e
+    | _ -> printf "No match"
