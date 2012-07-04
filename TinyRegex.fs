4ad76e6cf6d08b2bed4249ec3dbf2a98bdb023d7
Eric Woroshow <eric@ericw.ca>
2012-07-04 00:00:22 -0700

    Added a tokenizer for expressions.

---
+++ TinyRegex.fs
@@ -0,0 +1,33 @@
+﻿// TinyRegex: A tiny regular matcher using derivatives in F#
+module TinyRegex
+
+// A regular expression is:
+// <regex> = <term> '|' <regex>
+//         | <term>
+// <term> = { <factor> }
+// <factor> = <base> [ '*' ]
+// <base> = <char>
+//        | '(' <regex> ')'
+
+type Regex =
+    | Empty
+    | Null
+    | Char of char
+    | Alt of Regex * Regex
+    | Seq of Regex * Regex
+    | Rep of Regex * Regex
+
+type Tokens =
+    | Char of char
+    | LParen
+    | RParen
+    | Star
+    | Pipe
+
+let tokenize (input : string) =
+    input |> Seq.map (function
+        | '(' -> LParen
+        | ')' -> RParen
+        | '*' -> Star
+        | '|' -> Pipe
+        | c -> Char c)
